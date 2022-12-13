3caff7a189ad58e1bfadd6f5fdcdbeada85d9e44
wobble-house <ross@wobble.house>
2022-12-13 11:37:23 -0500

    updating providers variable

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,10 +2,9 @@
 
 import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink} from "@apollo/client"
 
-
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: process.env.NEXT_PUBLIC_A2IMCMS_API_GQL_URL
+  uri: `${process.env.NEXT_PUBLIC_A2IMCMS_API_GQL_URL}`
 });
 const client = new ApolloClient({
   cache,
@@ -13,8 +12,6 @@ const client = new ApolloClient({
 });
 
 export default client;
-
-
 
 export function Providers({ children }) {
   return (
