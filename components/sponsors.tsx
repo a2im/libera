7e043087eb761f0cabfdbf54e6b85d45cff769fa
wobble.house <ross@wobble.house>
2023-05-04 20:27:27 -0400

    updating sponsor footer

--- components/sponsors.tsx
+++ components/sponsors.tsx
@@ -5,6 +5,38 @@ import { useQuery } from '@apollo/client';
 import Link from "next/link";
 import Image from "next/image";
 
+export function SponsorTitle() {
+  const { loading, error, data } = useQuery(GET_ALL_SPONSORS, { 
+    variables: {
+      Level: "Title Sponsor", 
+      Title: "2023 Libera Awards"
+    }});
+    if (loading) return <div className="animate-pulse h-[300px] w-[300px] bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+    if (error) return <p>Error</p>
+  return (
+      <div className="max-w-xl relative py-5 flex mx-auto justify-evenly gap-10">
+      {/* Map through the data */}
+      {data?.sponsors.data.map(sponsors => (
+              <div key={sponsors.id} className="flex relative hover:scale-105 justify-evenly">
+                <Link href={sponsors.attributes?.URL} target="_blank" rel="noopener noreferrer">
+                  <div className="relative w-[300px] h-[200px] mx-auto justify-evenly">
+                <Image 
+                  src={sponsors.attributes.Logo.data.attributes.url}
+                  fill={true}
+                  alt={sponsors.attributes.Logo.data.attributes.alternativeText} 
+                  style={{ objectFit: 'contain'}}
+                  sizes="(max-width: 768px) 100vw,
+                        (max-width: 1200px) 50vw,
+                        33vw"
+                  />
+                  </div>
+                  </Link>
+              </div>
+            )
+          )}
+      </div>
+  );
+}
 
 export function SponsorTier1() {
     const { loading, error, data } = useQuery(GET_ALL_SPONSORS, { 
@@ -15,10 +47,10 @@ export function SponsorTier1() {
       if (loading) return <div className="animate-pulse h-[300px] w-[300px] bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
       if (error) return <p>Error</p>
     return (
-        <div className="relative py-5 flex flex-col md:flex-row mx-auto justify-evenly gap-10">
+        <div className="relative py-5 grid grid-cols-0 md:grid-cols-2 mx-auto justify-evenly gap-10">
         {/* Map through the data */}
         {data?.sponsors.data.map(sponsors => (
-                <div key={sponsors.id} className="relative hover:scale-105 justify-evenly">
+                <div key={sponsors.id} className="flex relative hover:scale-105 justify-evenly">
                   <Link href={sponsors.attributes?.URL} target="_blank" rel="noopener noreferrer">
                     <div className="relative w-[300px] h-[200px] mx-auto justify-evenly">
                   <Image 
@@ -80,7 +112,7 @@ export function SponsorTier3() {
       if (loading) return <div className="animate-pulse h-[128px] w-[128px] bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
       if (error) return <p>Error</p>
     return (
-        <div className="relative pb-20 py-5 flex flex-col md:flex-row gap-20 mx-auto">
+        <div className="relative pb-20 py-5  flex flex-col md:flex-row  gap-20 mx-auto">
         {/* Map through the data */}
         {data?.sponsors?.data.map(sponsors => (
                 <div key={sponsors.id} className="mx-auto relative hover:scale-105 w-[128px] h-[128px]">
@@ -117,6 +149,8 @@ export default function SponsorFooter () {
     <div className="flex flex-col max-w-6xl mx-auto p-8 bg-sky-50 shadow-2xl rounded-2xl text-center">
       <h3 className="max-w-xl text-center mx-auto">Our Sponsors</h3>
       <hr className="mt-3 mb-5 Hrswap"></hr>
+        <SponsorTitle/>
+        <hr className="mb-8 Hrswap"></hr>
         <SponsorTier1/>
         <hr className="mb-8 Hrswap"></hr>
         <SponsorTier2/>
